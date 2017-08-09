8438c245e7884da81b98a76ee8871020442592bd
elkmare <elkmare@gmail.com>
2017-08-09 13:09:40 +1000

    Ass 'About' as a component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './AppTest2.css';
 import {SweetDevilPic} from './SweetDevilPic';
 import {SweetDevilIden} from './SweetDevilIden';
+import {SweetDevilAbout} from './SweetDevilAbout';
 
 class Test extends Component {
   render () {
@@ -23,17 +24,9 @@ class Test extends Component {
         <div className="kolumnaPrawa">
             <SweetDevilIden />
         <br />
-        <table className="iden">
-        <tbody>
-            <tr>
-                <th>About</th>
-            </tr> 
-            <tr>
-                <td id="about">Her True Name is Altera (アルテラ,), better known in history as Attila the Hun (アッティラ・ザ・フン). She was the overlord who established a great empire in the 5th Century. She was a warrior and king of the Huns, a tribe descending from the Xiongnu (Hunni). Leading an army of cavalry, the conquests of this great hero covered a great part of the map, controlling a vast territory spanning from Western Asia to Russia, Eastern Europe and even Gaul. It can be said that she invited the eventual collapse of the Roman Empire. Throughout her life, Altera was always in the midst of battle. Due to the terrifying acts she commited in war, she was greatly feared in all European nations as the Scourge of God (神の懲罰) and the Whip of God (神の鞭). She is a pure King of Combat (戦闘王).</td>
-            </tr>
-        </tbody>
-        </table>
-    <br />
+            <SweetDevilAbout />
+        <br />
+
             <table className="commands">
                 <tbody>
                     <tr>
---
+++ src/SweetDevilAbout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export class SweetDevilAbout extends Component {
+  render() {
+    return(
+    <table className="about"> 
+      <tr>
+        <th>About</th>
+      </tr> 
+      <tr>
+        <td id="about">Her True Name is Altera (アルテラ,), better known in history as Attila the Hun (アッティラ・ザ・フン). She was the overlord who established a great empire in the 5th Century. She was a warrior and king of the Huns, a tribe descending from the Xiongnu (Hunni). Leading an army of cavalry, the conquests of this great hero covered a great part of the map, controlling a vast territory spanning from Western Asia to Russia, Eastern Europe and even Gaul. It can be said that she invited the eventual collapse of the Roman Empire. Throughout her life, Altera was always in the midst of battle. Due to the terrifying acts she commited in war, she was greatly feared in all European nations as the Scourge of God (神の懲罰) and the Whip of God (神の鞭). She is a pure King of Combat (戦闘王).</td>
+      </tr>
+    </table>  
+    )
+  }
+}
