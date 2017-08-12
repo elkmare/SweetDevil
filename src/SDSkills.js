290fa6811748384adeb263d73e6f88d620270ddf
elkmare <elkmare@gmail.com>
2017-08-13 00:12:56 +1000

    Add URLS to Skills

--- src/SDSkills.js
+++ src/SDSkills.js
@@ -2,39 +2,7 @@ import React, { Component } from 'react';
 import {Skill} from './Skill';
 import {TableHeader} from './Header';
 import {TableName} from './Name';
-import {SDSkillsActive} from './SDSkillsActive';
-import {SDSkillsPassive} from './SDSkillsPassive';
 import {SDSkillsGroup} from './SDSkillsGroup';
-
-const Marksmanship = {
-  src: 'http://fate-go.cirnopedia.org/icons/skill/skill_017.png',
-  alt: 'Marksmanship'
-}
-
-const PresenceDet = {
-  src: 'http://fate-go.cirnopedia.org/icons/skill/skill_001.png',
-  alt: 'Presence Detection'
-}
-
-const Knight = {
-  src: 'http://fate-go.cirnopedia.org/icons/skill/skill_005.png',
-  alt: 'Knight Tactics'
-}
-
-const PresenceCon = {
-  src: 'http://fate-go.cirnopedia.org/icons/skill/skill_028.png',
-  alt: 'Presence Concealment'
-}
-
-const Experience = {
-  src: 'http://fate-go.cirnopedia.org/icons/skill/skill_027.png',
-  alt: 'Experience Point Bonus'
-}
-
-const Cosmo = {
-  src: 'http://fate-go.cirnopedia.org/icons/skill/skill_033.png',
-  alt: 'Cosmo Reactor'
-}
 
 export class SDSkills extends Component {
   render() {
@@ -43,14 +11,14 @@ export class SDSkills extends Component {
         <TableHeader name="Skills"/>
         <div className="skillsType">
           <SDSkillsGroup type="Active Skills">
-            <Skill name="Marksmanship" rank="A+" src={Marksmanship.src}/>
-            <Skill name="Presence Detection" rank="D+" src={PresenceDet.src}/>
-            <Skill name="Knight Tactics" rank="C+" src={Knight.src}/>
+            <Skill name="Marksmanship" rank="A+"/>
+            <Skill name="Presence Detection" rank="D+"/>
+            <Skill name="Knight Tactics" rank="C+"/>
           </SDSkillsGroup>
           <SDSkillsGroup type="Passive Skills">
-            <Skill name="Presence Concealment" rank="C-" src={PresenceCon.src}/>
-            <Skill name="Experience Point Bonus" rank="B-" src={Experience.src}/>
-            <Skill name="Cosmo Reactor" rank="A+" src={Cosmo.src}/>
+            <Skill name="Presence Concealment" rank="C-"/>
+            <Skill name="Experience Point Bonus" rank="B-"/>
+            <Skill name="Cosmo Reactor" rank="A+"/>
           </SDSkillsGroup>
         </div>
       </div>
--- src/Skill.js
+++ src/Skill.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
+const URLS = {
+  'Experience Point Bonus': 'http://fate-go.cirnopedia.org/icons/skill/skill_027.png',
+  'Marksmanship': 'http://fate-go.cirnopedia.org/icons/skill/skill_017.png',
+  'Presence Detection': 'http://fate-go.cirnopedia.org/icons/skill/skill_001.png',
+  'Knight Tactics': 'http://fate-go.cirnopedia.org/icons/skill/skill_005.png',
+  'Presence Concealment': 'http://fate-go.cirnopedia.org/icons/skill/skill_028.png',
+  'Cosmo Reactor': 'http://fate-go.cirnopedia.org/icons/skill/skill_033.png',
+}
+
 export class Skill extends Component {
   render() {
+    const url = URLS[this.props.name];
     return (
         <div>
           <div className="cellMargin">{this.props.name} {this.props.rank}</div>
           <div className="cellMargin">
             <img 
-              src={this.props.src}
+              src={url}
               alt={this.props.name} />
           </div>
         </div>
