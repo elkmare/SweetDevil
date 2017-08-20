6b7bea272316fdc86bf3be8f67505a68cabde972
elkmare <elkmare@gmail.com>
2017-08-20 19:42:26 +1000

    Add select

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import {PageContent} from './PageContent';
 import {Menu} from './Menu';
-import {AlteraSweetDevil} from './servants';
-import {Gilgamesh} from './servants';
+import * as servants from './servants';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentServant: 'Gilgamesh' };
+    this.changeName = this.changeName.bind(this);
+  }
+  
+  changeName(newName) {
+    this.setState({
+      currentServant: newName
+    });
+  }
+
+  getServant () {
+    const servant = servants[this.state.currentServant];
+    return servant;
+  }
+
   render() {
     return (
-      <div className='fullPage'>
-      <Menu />
-      <PageContent servant={Gilgamesh} />
+      <div className="fullPage">
+        <Menu onChange={this.changeName} />
+        <PageContent servant={this.getServant()} />
       </div>
     )
   }
--- src/Menu.js
+++ src/Menu.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 
 export class Menu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e){
+    const name = e.target.value;
+    this.props.onChange(name);
+  }
+
   render () {
     return (
-      <div>dupa
+      <div clasName="menuContent">
+        <div className="choose">Choose servant:</div>
+        <div className="select">
+          <select onChange={this.handleChange}>
+            <option value="Gilgamesh">Gilgamesh</option>
+            <option value="AlteraSweetDevil">Altera Sweet Devil</option>
+          </select>
+        </div>
 
-
-
-
-        
       </div>
     )
   }
